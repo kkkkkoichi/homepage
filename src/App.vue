32cef5073e1b03d8db3b7c44515cd12d5a1c7a6f
FMVA53SW\Owner <kouk0202@gmail.com>
2021-08-13 15:56:19 +0900

    テンプレ破棄

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <core-app-bar />
+
+    <core-view />
+
+    <core-footer />
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  export default {
+    name: 'App',
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+    components: {
+      CoreAppBar: () => import('@/components/core/AppBar'),
+      CoreFooter: () => import('@/components/core/Footer'),
+      CoreView: () => import('@/components/core/View'),
+    },
   }
-}
-</style>
+</script>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,57 @@
+<template>
+  <section
+    id="welcome"
+    class="overflow-hidden"
+  >
+    <v-row no-gutters>
+      <v-col
+        class="hidden-sm-and-down"
+        md="6"
+      >
+        <v-img
+          :src="require('@/assets/welcome.png')"
+          height="100vh"
+        />
+      </v-col>
+
+      <v-col
+        class="align-content-space-between layout wrap"
+        cols="12"
+        md="6"
+        :pa-5="$vuetify.breakpoint.smAndDown"
+      >
+        <base-bubble-1
+          style="transform: rotate(180deg) translateY(25%)"
+        />
+
+        <v-row
+          align="center"
+          justify="center"
+          class="pl-10"
+        >
+          <v-col
+            cols="10"
+            md="6"
+            class="ma-5"
+            align="center"
+          >
+            <base-heading>Welcome!</base-heading>
+            <base-text>
+              kkkkkkkoichiです！aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<br>
+            </base-text>
+            <base-btn class="mt-4">
+              よかったら下も見てね。
+            </base-btn>
+          </v-col>
+        </v-row>
+
+        <base-bubble-2
+          style="transform: rotate(180deg) translate(-200px, -15%)"
+        />
+      </v-col>
+    </v-row>
+  </section>
+</template>
+<style scoped>
+
+</style>
